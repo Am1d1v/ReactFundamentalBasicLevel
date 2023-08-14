41d2b18054810d8489dcdd230df16cd5c0dafdad
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-14 19:55:37 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ import App from './App';
 
 
 //const elem = <h2>Some start text</h2>;
-  const elem = React.createElement('h1', null, "Some text in React Element");
+//const elem = React.createElement('h1', {className: 'greeting'}, "Some text in React Element");
+  const elem = (
+    <div>
+        <h1>"Some h1 text"</h1>
+        <input type='text' />
+        <button> Button to click</button>
+    </div>
+  );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
