647558a510dd4bb7d6288ebaf2ea8209186dbd90
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-14 22:09:41 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,37 @@
 import './App.css';
 
-function App() {
+
+const Header = function(){
+  return (<h2>Some Header Text</h2>)
+};
+
+const Field =  function(){
+  return(
+    <input placeholder='Type here' type='text'/>
+  )
+}
+
+function Btn(){
+  const text = "Log In";
+  const res = () => {
+    return 'Logged In';
+  }
+  return(
     
-  
+    <button>{res()}</button>
+  )
+}
+
+
+function App() {
+    return(
+      <div className="App">
+        <Header />
+        <Field />
+        <Btn />
+
+      </div>
+    ); 
 }
 
 export default App;
--- index.js
+++ index.js
@@ -5,18 +5,10 @@ import App from './App';
 
 
 
-//const elem = <h2>Some start text</h2>;
-//const elem = React.createElement('h1', {className: 'greeting'}, "Some text in React Element");
-  const elem = (
-    <div>
-        <h1>"Some h1 text"</h1>
-        <input type='text' />
-        <button> Button to click</button>
-    </div>
-  );
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    elem
+    <App />
 );
 
